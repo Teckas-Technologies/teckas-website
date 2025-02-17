9b4c132d027b0bef6e7976c289e40ab13829358a
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-02-17 22:24:35 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,1 +1,14 @@
 
+import React from "react";
+import "./Button.css";
+
+const Button = () => {
+  return (
+    <div className="app">
+      <button className="gradient-button">Succes needs ur wish</button>
+    </div>
+  );
+};
+
+export default Button;
+
