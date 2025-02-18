fd8afa2fb9df2c49cee2282701b0279ed0e37649
sabzdotpy <incrediblesabari02@gmail.com>
2025-02-18 18:14:17 +0530

    setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from "react";
 import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom";
 
 
-import React from "react";
+import Home from "./Pages/Home/Home";
 import Button from "./components/Button";
 import OurServiceCard from "./components/OurServiceCard";
 import Grid from "./components/Grid";
 import Footer from "./Pages/Footer";
 // import Sec from "./Pages/Sec/Sec";
-import "./App.css"
 
 const App: React.FC = () => {
   return (
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Footer />} />
+      </Routes>
+    </Router>
+
    
-      <div>
-    <Grid/>
-       <Button />
-        <OurServiceCard /> 
-        {/* <Sec/> */}
-       <Footer /> 
-      </div>
+    //   <div>
+    // <Grid/>
+    //    <Button />
+    //     <OurServiceCard /> 
+    //     {/* <Sec/> */}
+    //    <Footer /> 
+    //   </div>
       
   );
 };
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.scss";
+import React from "react";
+
+const Home: React.FC = () => {
+    return (
+        <div className="Home">
+            Home Page
+        </div>
+    );
+}
+
+export default Home;
